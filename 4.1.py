8c1adf972f46abb28e89a8f32c6ed54e20e08fe6
ScOoOpie <113428677+ScOoOpie@users.noreply.github.com>
2022-09-21 09:22:34 +0300

    Update 4.1.py

--- 4.1.py
+++ 4.1.py
@@ -3,8 +3,8 @@ import math
 x = float(input("x = "))
 
 
-def f(x):
+def f(_x):
     return (x + math.sin (x)) / math.log10 (math.fabs (x-math.pow (x,4))) + math.log (x, 4)
 
 
-print(f(x))
+print(f(_x))
